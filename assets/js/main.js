0f55fc693ca6fde2136d36ef8451544f1a9f0732
Luck1080p <lucasbcontato@outlook.com>
2026-04-17 12:47:46 -0300

    v2

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,10 @@ function entrar() {
   document.getElementById('splash').classList.add('hidden');
   const main = document.getElementById('main');
   main.style.display = 'block';
+  
   // Pequeno delay para o display:block ter efeito antes da animação
   setTimeout(() => { 
-    main.style.opacity = '1'; 
-  }, 10);
+    main.classList.add('visible');
+  }, 50);
 }
+
